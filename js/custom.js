a490baafe420cdf54ecffd28dce37c1c5000a155
Albert <albertmartin2000@gmail.com>
2022-07-05 16:38:57 +0200

    Home full screen and simplified

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,12 @@ $(function()
 {
     new WOW().init();
     $(".rotate").textrotator();
+	$(".element").each(function () {
+        var e = $(this);
+        e.typed({
+                strings: e.attr("data-elements").split(","),
+                typeSpeed: 100,
+                backDelay: 3e3
+        })
+	});
 })
