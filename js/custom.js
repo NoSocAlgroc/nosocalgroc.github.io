c439c32d2ab57f9c7c0e92226b5f1473403dc623
Albert <albertmartin2000@gmail.com>
2022-07-10 19:12:47 +0200

    Basic coding language carousel
    Added Font Awesome as well.

--- js/custom.js
+++ js/custom.js
@@ -99,3 +99,10 @@ document.addEventListener("keydown", (function (t) {
 		t.ctrlKey && t.shiftKey && 73 == t.keyCode && e(t), t.ctrlKey && t.shiftKey && 74 == t.keyCode && e(t), 83 == t.keyCode && (navigator.platform.match("Mac") ? t.metaKey : t.ctrlKey) && e(t), t.ctrlKey && 85 == t.keyCode && e(t), 123 == event.keyCode && e(t)
 	}), !1)
 };
+
+$(window).on("load",function(){
+	$(".carousel-item").each(function(e)
+	{
+		console.log(e);
+	});
+})
